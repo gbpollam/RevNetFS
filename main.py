Fixed backprop for ConvLayer with SAME padding

main.py
@@ -33,23 +33,26 @@ STEP_DISTANCE = 10
 
 def main():
     file_path = 'dataset/WISDM_ar_v1.1_raw.txt'
-    x_train, y_train_hot, x_test, y_test_hot = prepare_data(file_path, TIME_PERIODS, STEP_DISTANCE)
+    x_train, y_train_hot, x_test, y_test_hot = prepare_data(file_path, TIME_PERIODS, STEP_DISTANCE, scaler_type='minmax')
 
     model = NeuralNetwork()
     model.add(ConvLayer(input_shape=(20, 3), kernel_size=3, num_filters=32, stride=1))
     model.add(ActivationLayer('relu'))
     model.add(AvgPool1d(window_size=2))
     model.add(ConvLayer(input_shape=(9, 32), kernel_size=3, num_filters=64, stride=1))
+    model.add(ActivationLayer('relu'))
+    model.add(ConvLayer(input_shape=(7, 64), kernel_size=3, num_filters=128, stride=1, padding='SAME'))
+    model.add(ActivationLayer('relu'))
     model.add(AvgPool1d(window_size=2))
     model.add(GAPLayer())
     # model.add(FCLayer(units=50, input_dim=64))
     # model.add(ActivationLayer('relu'))
-    model.add(FCLayer(units=6, input_dim=64))
+    model.add(FCLayer(units=6, input_dim=128))
     model.add(ActivationLayer('softmax'))
 
     model.set_loss(tf.keras.losses.BinaryCrossentropy())
 
-    model.fit(x_train, y_train_hot, batch_size=32, epochs=10, learning_rate=0.01)
+    model.fit(x_train, y_train_hot, batch_size=1, epochs=10, learning_rate=0.01)
 
     predictions = model.predict(x_test)
     true_preds = []

models/NeuralNetwork.py
@@ -4,6 +4,8 @@ from tqdm import tqdm
 import math
 import time
 
+from utils.prepare_data import shuffle_data
+
 
 class NeuralNetwork:
     def __init__(self):
@@ -34,7 +36,7 @@ class NeuralNetwork:
 
     def sgd(self, x_train, y_train, epochs, learning_rate):
         for epoch in range(epochs):
-            x_train = tf.random.shuffle(x_train)
+            x_train, y_train = shuffle_data(x_train, y_train)
             tot_loss = 0
             # tqdm(range(x_train.get_shape()[0]), desc=("Training epoch ", epoch, " of ", epochs))
             print("Training epoch ", (epoch+1), " of ", epochs, ":")
@@ -71,11 +73,7 @@ class NeuralNetwork:
 
     def minibatch_gd(self, x_train, y_train, batch_size, epochs, learning_rate):
         for epoch in range(epochs):
-
-            indices = tf.range(start=0, limit=tf.shape(x_train)[0], dtype=tf.int32)
-            shuffled_indices = tf.random.shuffle(indices)
-            x_train = tf.gather(x_train, shuffled_indices)
-            y_train = tf.gather(y_train, shuffled_indices)
+            x_train, y_train = shuffle_data(x_train, y_train)
             tot_loss = 0
             # tqdm(range(x_train.get_shape()[0]), desc=("Training epoch ", epoch, " of ", epochs))
             print("Training epoch ", (epoch+1), " of ", epochs, ":")
@@ -86,7 +84,7 @@ class NeuralNetwork:
                 target = y_train[i]
                 # fw_times = []
                 for layer in self.layers:
-                    start = time.time()
+                    # start = time.time()
                     if layer.needs_inputs():
                         self.inputs[layer.id] = output
                     output = layer.forward(output)